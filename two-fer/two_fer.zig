205fe5ba8438b020d3ac91a1d774a5946a96e439
MichaelFisher1997 <contact@micahelfisher.tech>
2024-09-22 01:18:49 +0100

    reduced code

--- two-fer/two_fer.zig
+++ two-fer/two_fer.zig
@@ -1,6 +1,4 @@
-const test_allocator = std.testing.allocator;
-const std = @import("std");
-const bufPrint = std.fmt.bufPrint;
+const bufPrint = @import("std").fmt.bufPrint;
 pub fn twoFer(buffer: []u8, name: ?[]const u8) anyerror![]u8 {
     const n = name orelse "you";
     return try bufPrint(buffer, "One for {s}, one for me.", .{n});
